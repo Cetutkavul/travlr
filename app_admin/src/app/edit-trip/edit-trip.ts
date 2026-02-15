d5da06b53e22851982bc449d69510e41e3d4422e
Cetutkavul <cetutkavul@gmail.com>
2026-02-14 18:57:08 -0600

    Module 6 completed baseline

---
+++ app_admin/src/app/edit-trip/edit-trip.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TripData } from '../services/trip-data';
+import { Trip } from '../models/trip';
+
+@Component({
+  selector: 'app-edit-trip',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './edit-trip.html',
+  styleUrl: './edit-trip.css'
+})
+export class EditTrip implements OnInit {
+
+  editForm!: FormGroup;
+  submitted = false;
+  tripCode = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private tripService: TripData,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+
+    this.tripCode = this.route.snapshot.paramMap.get('code') || '';
+
+    this.editForm = this.fb.group({
+      code: ['', Validators.required],
+      name: ['', Validators.required],
+      length: ['', Validators.required],
+      start: ['', Validators.required],
+      resort: ['', Validators.required],
+      perPerson: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+
+    this.loadTrip();
+  }
+
+private loadTrip(): void {
+  this.tripService.getTrip(this.tripCode).subscribe({
+    next: (tripArray: Trip[]) => {
+
+      if (tripArray.length === 0) {
+        alert('Trip not found');
+        this.router.navigate(['']);
+        return;
+      }
+
+      const trip = tripArray[0];
+
+      // ⭐ FIX DATE FORMAT
+      const formattedTrip = {
+        ...trip,
+        start: trip.start
+          ? new Date(trip.start).toISOString().substring(0, 10)
+          : ''
+      };
+
+      this.editForm.patchValue(formattedTrip);
+    },
+    error: err => console.error(err)
+  });
+}
+
+  onSubmit(): void {
+
+    this.submitted = true;
+
+    if (this.editForm.invalid) return;
+
+    this.tripService.updateTrip(this.tripCode, this.editForm.value)
+      .subscribe({
+        next: () => {
+          alert('Trip updated!');
+          this.router.navigate(['']);
+        },
+        error: err => console.error(err)
+      });
+  }
+
+  get f() {
+    return this.editForm.controls;
+  }
+}
--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -6,39 +6,15 @@ const Model = mongoose.model('trips');
 // Regardless of outcome, response must include HTML status code
 // and JSON message to the requesting client
 const tripsList = async (req, res) => {
-    const q = await Model
-        .find({}) // No filter, return all records
-        .exec();
-
-    // Uncomment the following line to show results of query
-    // on the console
-    // console.log(q);
-
-    if (!q) { // Database returned no data
-        return res
-            .status(404)
-            .json(err);
-    } else { // Return resulting trip list
-        return res
-            .status(200)
-            .json(q);
-    }
-};
-
-// GET: /trips/:tripCode - lists a single trip
-// Regardless of outcome, response must include HTML status code
-// and JSON message to the requesting client
-const tripsFindByCode = async(req, res) => {
   const q = await Model
-    .find({ 'code' : req.params.tripCode }) // Return single record
+    .find({}) // No filter, return all records
     .exec();
 
   // Uncomment the following line to show results of query
   // on the console
   // console.log(q);
 
-  if(!q)
-  { // Database returned no data
+  if (!q) { // Database returned no data
     return res
       .status(404)
       .json(err);
@@ -49,9 +25,103 @@ const tripsFindByCode = async(req, res) => {
   }
 };
 
-module.exports = {
-    tripsList,
-    tripsFindByCode
+// GET: /trips/:tripCode - lists a single trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsFindByCode = async (req, res) => {
+  const q = await Model
+    .find({ 'code': req.params.tripCode }) // Return single record
+    .exec();
+
+  // Uncomment the following line to show results of query
+  // on the console
+  // console.log(q);
+
+  if (!q) { // Database returned no data
+    return res
+      .status(404)
+      .json(err);
+  } else { // Return resulting trip list
+    return res
+      .status(200)
+      .json(q);
+  }
 };
 
+// POST: /trips - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async (req, res) => {
 
+  const newTrip = new Trip({
+    code: req.body.code,
+    name: req.body.name,
+    length: req.body.length,
+    start: req.body.start,
+    resort: req.body.resort,
+    perPerson: req.body.perPerson,
+    image: req.body.image,
+    description: req.body.description
+  });
+
+  const q = await newTrip.save();
+
+  if (!q) {
+    // Database returned no data
+    return res
+      .status(400)
+      .json(err);
+  } else {
+    // Return new trip
+    return res
+      .status(201)
+      .json(q);
+  }
+
+  // Uncomment the following line to show results of operation
+  // on the console
+  // console.log(q);
+};
+
+// PUT: /trips/:tripCode - Adds a new Trip 
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client 
+const tripsUpdateTrip = async (req, res) => {
+
+  try {
+    const updatedTrip = await Model.findOneAndUpdate(
+      { code: req.params.tripCode },
+      {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+      },
+      { new: true }   // return updated record
+    );
+
+    if (!updatedTrip) {
+      return res.status(404).json({ message: "Trip not found" });
+    }
+
+    return res.status(200).json(updatedTrip);
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
+  }
+};
+  // Uncomment the following line to show results of operation
+  // on the console 
+  // console.log(q); 
+
+module.exports = {
+  tripsList,
+  tripsFindByCode,
+  tripsAddTrip,
+  tripsUpdateTrip
+};
